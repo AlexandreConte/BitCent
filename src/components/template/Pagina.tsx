91f4c0d58e7df7e2ff6d457830f75aff46491af3
Alexandre Conte <alexandreconte.dev@gmail.com>
2023-12-23 12:27:47 -0300

    forcando autenticacao

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -1,3 +1,5 @@
+import ForcarAutenticacao from "../autenticacao/ForcarAutenticacao"
+
 interface PaginaProps {
     externa?: boolean
     className?: string
@@ -5,15 +7,24 @@ interface PaginaProps {
 }
 
 export default function Pagina({ externa, className, children }: PaginaProps) {
-    return (
-        <div
-            className={`
-                flex flex-col min-h-screen
-                bg-gradient-to-r from-zinc-900 via-black to-zinc-900
-                ${className ?? ""}
-            `}
-        >
-            {children}
-        </div>
+
+    function renderizar() {
+        return (
+            <div
+                className={`
+                    flex flex-col min-h-screen
+                    bg-gradient-to-r from-zinc-900 via-black to-zinc-900
+                    ${className ?? ""}
+                `}
+            >
+                {children}
+            </div>
+        )
+    }
+
+    return externa ? renderizar() : (
+        <ForcarAutenticacao>
+            {renderizar()}
+        </ForcarAutenticacao>
     )
 }
